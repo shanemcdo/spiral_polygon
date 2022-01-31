57b5233cff4594ee568ea82c97d814248e417e44
Shane McDonough <shanemcdman@gmail.com>
2022-01-30 23:29:00 -0800

    Allow toggleable sliders

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let els = {
     'delta_theta': document.querySelector('#delta-theta'),
     'stroke_weight': document.querySelector('#stroke-weight'),
     'max_length': document.querySelector('#max-length'),
+    'checkboxes': {
+        'slider': document.querySelector('#slider-checkbox'),
+    },
 };
 let delta_theta = 360 / els.sides.value - 1;
 
@@ -10,7 +13,7 @@ function setup(){
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
     els.sides.addEventListener('input', ()=>{
-        let val = parseInt(els.sides.value);
+        let val = parseFloat(els.sides.value);
         if(val < els.sides.min)
             els.sides.value = els.sides.min;
         reset_some();
@@ -18,13 +21,21 @@ function setup(){
     });
     els.delta_theta.addEventListener('input', redraw);
     els.stroke_weight.addEventListener('input', ()=>{
-        let val = parseInt(els.stroke_weight.value);
+        let val = parseFloat(els.stroke_weight.value);
         if(val < els.stroke_weight.min)
             els.stroke_weight.value = els.stroke_weight.min;
         strokeWeight(els.stroke_weight.value);
         redraw();
     });
     els.max_length.addEventListener('input', redraw);
+    els.checkboxes.slider.addEventListener('input', ()=>{
+        let type = "number";
+        if(els.checkboxes.slider.checked)
+            type = 'range';
+        els.delta_theta.type = type;
+        els.stroke_weight.type = type;
+        els.max_length.type = type;
+    });
     reset_all();
     noLoop();
 }
@@ -38,9 +49,9 @@ function draw(){
     translate(windowWidth / 2, windowHeight / 2);
     let pos = createVector();
     let angle = 0;
-    let sides = parseInt(els.sides.value);
-    let max_size = parseInt(els.max_length.value);
-    let delta_theta = parseInt(els.delta_theta.value);
+    let sides = parseFloat(els.sides.value);
+    let max_size = parseFloat(els.max_length.value);
+    let delta_theta = parseFloat(els.delta_theta.value);
     for(let i = 0; i < max_size; i++){
         stroke(calculate_rainbow(i % sides, sides));
         pos = draw_line_at_angle(pos, angle, i);
@@ -71,7 +82,7 @@ function calculate_rainbow(current, max) {
 }
 
 function reset_some(){
-    els.delta_theta.value = 360 / parseInt(els.sides.value) - 1;
+    els.delta_theta.value = 360 / parseFloat(els.sides.value) - 1;
 }
 
 function reset_all(){
